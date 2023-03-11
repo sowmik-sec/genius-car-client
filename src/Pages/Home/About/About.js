e2db2b1ddd317616d7e3fbb1615c6a21760ff1f8
sowmik-sec <sowmik.sec@gmail.com>
2023-03-11 16:01:34 +0600

    fake data created and loaded in services and then showing services with responsive system

--- src/Pages/Home/About/About.js
+++ src/Pages/Home/About/About.js
@@ -4,7 +4,7 @@ import parts from "../../../assets/images/about_us/parts.jpg";
 
 const About = () => {
   return (
-    <div className="hero min-h-screen bg-base-200">
+    <div className="hero my-20">
       <div className="hero-content flex-col lg:flex-row">
         <div className="relative w-1/2">
           <img
@@ -20,7 +20,7 @@ const About = () => {
         </div>
         <div className="w-1/2">
           <h3 className="text-2xl font-bold text-orange-500">About Us</h3>
-          <h1 className="text-5xl font-bold">
+          <h1 className="my-5 text-5xl font-bold">
             We are qualified <br /> & of experience in <br /> this field
           </h1>
           <p className="py-6">
---
+++ src/Pages/Home/Services/Services.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+import ServiceCard from "./ServiceCard";
+
+const Services = () => {
+  const [services, setServices] = useState([]);
+  useEffect(() => {
+    fetch("services.json")
+      .then((res) => res.json())
+      .then((data) => setServices(data));
+  }, []);
+  return (
+    <div>
+      <div className="text-center mb-4">
+        <p className="text-2xl font-bold text-orange-600">Services</p>
+        <div className="text-5xl font-semibold">Our Service Area</div>
+        <p className="w-1/2 mx-auto">
+          The majority have suffered alteration in some form, by injected
+          humour, or randomised words which don't look even slightly believable.{" "}
+        </p>
+      </div>
+      <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        {services.map((service) => (
+          <ServiceCard key={service._id} service={service} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Services;
