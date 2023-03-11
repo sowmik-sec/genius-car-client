e57ae26ede2f2fd0db2dac3fcfacdac0a1907274
sowmik-sec <sowmik.sec@gmail.com>
2023-03-11 16:19:28 +0600

    arrow icon added to card

--- src/Pages/Home/Services/ServiceCard.js
+++ src/Pages/Home/Services/ServiceCard.js
@@ -13,7 +13,20 @@ const ServiceCard = ({ service }) => {
           Price: ${price}
         </p>
         <div className="card-actions justify-end">
-          <button className="btn btn-primary">Buy Now</button>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6 text-orange-600"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
+            />
+          </svg>
         </div>
       </div>
     </div>
