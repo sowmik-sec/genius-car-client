2f938f564ca7fbc3e5dcfcdc5afcfb44ac21b103
sowmik-sec <sowmik.sec@gmail.com>
2023-03-11 11:30:53 +0600

    main layer created

--- src/Router/Routes/Routes.js
+++ src/Router/Routes/Routes.js
@@ -1,8 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
+import Main from "../../layout/Main";
+import Home from "../../Pages/Home/Home/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
+    element: <Main />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
   },
 ]);
 export default router;
