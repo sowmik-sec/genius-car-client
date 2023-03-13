aa44daef117e98b7a7d4dfe83a80ef262b4b28ea
sowmik-sec <sowmik.sec@gmail.com>
2023-03-13 15:23:06 +0600

    making orders page

--- src/Pages/Orders/Orders.js
+++ src/Pages/Orders/Orders.js
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../contexts/AuthProvider/AuthProvider";
+import OrderRow from "./OrderRow";
 
 const Orders = () => {
   const { user } = useContext(AuthContext);
@@ -13,6 +14,29 @@ const Orders = () => {
   return (
     <div>
       <h2 className="text-5xl">You have {orders.length} orders</h2>
+      <div className="overflow-x-auto w-full">
+        <table className="table w-full">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>
+                <label>
+                  <input type="checkbox" className="checkbox" />
+                </label>
+              </th>
+              <th>Name</th>
+              <th>Job</th>
+              <th>Favorite Color</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {orders.map((order) => (
+              <OrderRow key={order._id} order={order} />
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
